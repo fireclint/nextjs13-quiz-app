d28238ff34ce1bb10c9cc11266d2e58983c548d6
Clint Briley <firebriley@Clints-MBP.home>
2023-05-27 17:03:59 -0500

    nextjs13-quiz-app

---
+++ app/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital of France?',
+        answers: ['Madrid', 'Paris', 'Rome', 'Berlin'],
+        correctAnswer: 'Paris',
+      },
+      {
+        id: 2,
+        question: 'What is the largest planet in our solar system?',
+        answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+        correctAnswer: 'Jupiter',
+      },
+      {
+        id: 3,
+        question: 'What is the smallest country in the world?',
+        answers: ['Monaco', 'Maldives', 'Vatican City', 'San Marino'],
+        correctAnswer: 'Vatican City',
+      },
+      {
+        id: 4,
+        question: 'What is the most widely spoken language in the world?',
+        answers: ['English', 'Mandarin', 'Spanish', 'Hindi'],
+        correctAnswer: 'Mandarin',
+      },
+      {
+        id: 5,
+        question: 'Who is the founder of Microsoft?',
+        answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
+        correctAnswer: 'Bill Gates',
+      },
+    ],
+  };
+  
---
+++ app/quiz/page.jsx
@@ -0,0 +1,116 @@
+'use client';
+import React, { useState } from 'react';
+import { quiz } from '../data.js';
+
+const page = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+
+  //   Select and check answer
+  const onAnswerSelected = (answer, idx) => {
+    setChecked(true);
+    setSelectedAnswerIndex(idx);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log('true');
+    } else {
+      setSelectedAnswer(false);
+      console.log('false');
+    }
+  };
+
+  // Calculate score and increment to next question
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+    setChecked(false);
+  };
+
+  return (
+    <div className='container'>
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>
+          Question: {activeQuestion + 1}
+          <span>/{questions.length}</span>
+        </h2>
+      </div>
+      <div>
+        {!showResult ? (
+          <div className='quiz-container'>
+            <h3>{questions[activeQuestion].question}</h3>
+            {answers.map((answer, idx) => (
+              <li
+                key={idx}
+                onClick={() => onAnswerSelected(answer, idx)}
+                className={
+                  selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
+                }
+              >
+                <span>{answer}</span>
+              </li>
+            ))}
+            {checked ? (
+              <button onClick={nextQuestion} className='btn'>
+                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+              </button>
+            ) : (
+              <button onClick={nextQuestion} disabled className='btn-disabled'>
+                {' '}
+                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+              </button>
+            )}
+          </div>
+        ) : (
+          <div className='quiz-container'>
+            <h3>Results</h3>
+            <h3>Overall {(result.score / 25) * 100}%</h3>
+            <p>
+              Total Questions: <span>{questions.length}</span>
+            </p>
+            <p>
+              Total Score: <span>{result.score}</span>
+            </p>
+            <p>
+              Correct Answers: <span>{result.correctAnswers}</span>
+            </p>
+            <p>
+              Wrong Answers: <span>{result.wrongAnswers}</span>
+            </p>
+            <button onClick={() => window.location.reload()}>Restart</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default page;
